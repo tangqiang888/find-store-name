eeb25ba24f69a53b430b86bb5e1ca6170f15a8ef
tangqiang888 <2230430939@qq.com>
2026-07-24 10:38:55 +0800

    Update search logic with keyword input and error handling
    Refactor search functionality to use keyword input and error handling.

--- miniprogram/pages/query/query.js
+++ miniprogram/pages/query/query.js
@@ -1,17 +1,30 @@
 const db = wx.cloud.database();
 Page({
-  data: { list: [] },
-  async onSearch(e) {
-    const keyword = e.detail.value.trim();
-    if (!keyword) { this.setData({ list: [] }); return; }
+  data: {
+    keyword: '',
+    list: []
+  },
+  onKeywordInput(e) {
+    this.setData({ keyword: e.detail.value });
+  },
+  async doSearch() {
+    const keyword = this.data.keyword.trim();
+    if (!keyword) {
+      this.setData({ list: [] });
+      return;
+    }
     wx.showLoading({ title: '搜索中...' });
-    const _ = db.command;
-    const res = await db.collection('goods_record').where(_.or([
-      { supplier: db.RegExp({ regexp: keyword, options: 'i' }) },
-      { goods_name: db.RegExp({ regexp: keyword, options: 'i' }) },
-      { order_time: db.RegExp({ regexp: keyword, options: 'i' }) }
-    ])).get();
-    this.setData({ list: res.data });
+    try {
+      const _ = db.command;
+      const res = await db.collection('goods_record').where(_.or([
+        { supplier: db.RegExp({ regexp: keyword, options: 'i' }) },
+        { goods_name: db.RegExp({ regexp: keyword, options: 'i' }) },
+        { order_time: db.RegExp({ regexp: keyword, options: 'i' }) }
+      ])).get();
+      this.setData({ list: res.data });
+    } catch (e) {
+      wx.showToast({ title: '搜索失败', icon: 'none' });
+    }
     wx.hideLoading();
   },
   viewImage(e) {
